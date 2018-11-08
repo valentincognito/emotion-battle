8a29607986dfa0bfd2fb04027d8ea29ed64c7b81
valentin <valentin.vannay@gmail.com>
2018-11-08 16:55:40 +0900

    Load pretrained model and try to predict

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const webcam = new Webcam(document.getElementById('webcam'))
+let model
 
 init()
 
@@ -7,5 +8,38 @@ async function init() {
     await webcam.setup()
   } catch (e) {
     console.log(e)
+  }
+
+  model = await tf.loadModel('models/model.json')
+
+  isPredicting = true
+  predict()
+}
+
+
+
+async function predict() {
+  //ui.isPredicting()
+  while (isPredicting) {
+    const predictedClass = tf.tidy(() => {
+      // Capture the frame from the webcam.
+      const img = webcam.capture()
+      console.log(img)
+
+      // Make a prediction through our newly-trained model using the activation
+      // from mobilenet as input.
+      const predictions = model.predict(img)
+
+      // Returns the index with the maximum probability. This number corresponds
+      // to the class the model thinks is the most probable given the input.
+      return predictions.as1D().argMax()
+    })
+
+    const classId = (await predictedClass.data())[0];
+    predictedClass.dispose();
+
+    console.log(classId)
+
+    await tf.nextFrame()
   }
 }
--- public/js/webcam.js
+++ public/js/webcam.js
@@ -33,14 +33,18 @@ class Webcam {
   capture() {
     return tf.tidy(() => {
       // Reads the image as a Tensor from the webcam <video> element.
-      const webcamImage = tf.fromPixels(document.getElementById('cropped_webcam'))
+      const webcamImage = tf.fromPixels(this.webcamElement)
 
       // Crop the image so we're using the center square of the rectangular
       // webcam.
       const croppedImage = this.cropImage(webcamImage)
+      console.log(croppedImage);
 
       // Expand the outer most dimension so we have a batch size of 1.
       const batchedImage = croppedImage.expandDims(0)
+
+      const reshaped = tf.reshape(batchedImage, [48, 48, 1])
+      console.log(reshaped);
 
       // Normalize the image between -1 and 1. The image comes in between 0-255,
       // so we divide by 127 and subtract 1.
@@ -54,8 +58,8 @@ class Webcam {
    */
   cropImage(img) {
     const size = Math.min(img.shape[0], img.shape[1])
-    const centerHeight = img.shape[0] / 2 //112
-    const beginHeight = centerHeight - (size / 2) //112 - 112
+    const centerHeight = img.shape[0] / 2
+    const beginHeight = centerHeight - (size / 2)
     const centerWidth = img.shape[1] / 2
     const beginWidth = centerWidth - (size / 2)
     return img.slice([beginHeight, beginWidth, 0], [size, size, 3])
